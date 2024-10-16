b2eabe80ad05c8f23a9365592b67fdd88c2ad77e
Kantanat Siripipatworakun <phantomktn@gmail.com>
2024-10-16 20:29:10 +0700

    fix some detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  // imports: [RouterOutlet, Box, ContactBox, EducationBox, ExperienceBox, InterestBox, IntroduceBox, SkillBox, SystemBox],
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  
+  title = 'poomktn-profile-ng'
 }
